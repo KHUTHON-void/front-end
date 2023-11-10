49b09d5d376298bdba1d353fced35d94043e2f29
송민석 <songms0909@khu.ac.kr>
2023-11-11 07:29:13 +0900

    Fix

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import { ReactComponent as MainLogo } from "../assets/G-R.svg";
+import { ReactComponent as MainText } from "../assets/text.svg";
 
 const BodyContainer = styled.div`
   background: #9c27b0;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
   padding: 20px;
   height: 100vh;
   width: 100vw;
 `;
 
 const Body = () => {
-  return <BodyContainer />;
+  return (
+    <>
+      <BodyContainer>
+        <MainLogo fill="#ffffff" />
+        <MainText />
+      </BodyContainer>
+    </>
+  );
 };
 
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+
 import khu_logo from "../assets/khu_logo.png";
 
 export const LandHeader = () => {
